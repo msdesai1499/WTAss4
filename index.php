0dce380a45c67476e11336200edf668b729018e2
Mandar Desai <66965756+msdesai1499@users.noreply.github.com>
2020-08-29 10:19:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+    <title>Menu Items</title>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <style type="text/css">
+    .header h1{
+        width: 100%;
+        background-color: lightslategray;
+        color: white;
+        font-family: 'Pacifico', cursive;
+        text-align: center;
+        padding: 15px;
+    }
+    
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Menu List of Food Items</h1>
+        </div>
+        <br>
+        <select id="drop" class="form-control">
+            <option>Select any Item</option>
+        </select>
+        <br><br>
+        <div class="profile"></div>
+    </div>
+    <script src="jquery-3.5.1.min.js"></script>
+	<script defer="true" src="script.js"></script>
+</body>
+</html>
